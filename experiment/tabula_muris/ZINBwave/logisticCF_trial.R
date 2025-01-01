4bef448e172e5ce8432f2176e4820c79265a4679
Mukai Wang <wangmk@umich.edu>
2025-01-01 15:06:22 -0500

    Add latest debug code for logisticCF.

--- experiment/tabula_muris/ZINBwave/logisticCF_trial.R
+++ experiment/tabula_muris/ZINBwave/logisticCF_trial.R
@@ -20,55 +20,57 @@ slice_0_validation[train_indices] <- 0
 
 slice_5 <- 1*(count_mat > 5)
 slice_10 <- 1*(count_mat > 10)
+slice_30 <- 1*(count_mat > 30)
+slice_100 <- 1*(count_mat > 100)
+slice_200 <- 1*(count_mat > 200)
+
 
 initial_mask <- matrix(1, nrow=nrow(count_mat),
                        ncol=ncol(count_mat))
 
-
-library(logisticPCA)
-# begin <- proc.time()
-# lpca_fit <- logisticPCA(x=slice_0, k=4, m=5)
-# end <- proc.time()
-# end - begin
-# lpca_probs <- fitted(lpca_fit, type="response")
-
-slice_10_0_train <- matrix(NA, nrow=nrow(slice_10), ncol=ncol(slice_10))
-slice_10_0_train[train_indices] <- slice_10[train_indices]
-
-slice_5_0_train <- matrix(NA, nrow=nrow(slice_5), ncol=ncol(slice_5))
-slice_5_0_train[train_indices] <- slice_5[train_indices]
-
-
+# fit with cxx implementation of logisticcf
 begin <- proc.time()
-lsvd_fit <- logisticSVD(x=slice_5_0_train, k=3)
-lsvd_probs <- fitted(lsvd_fit, type="response")
+result_cxx <- logisticcf(slice_5, slice_0, 4, 10, 1000, 1e-5, 10)
+# cxx_validation_probs <- result_0_cxx$pi[validation_indices]
 end <- proc.time()
-end - begin
-lsvd_probs_validation <- lsvd_probs[validation_indices]
+end-begin
 
-
+# fit with R implementation of logisticcf
 begin <- proc.time()
-lcf_fit <- logisticcf(X=slice_5, Z=slice_0_train, K=3, lambda=0.09, tol=1e-5)
-lcf_probs <- lcf_fit$pi
-lcf_probs_validation <- lcf_probs[validation_indices]
+result_0_r <- logisticcfR(slice_5, slice_0, K=4, lambda=10, tol=1e-5)
+# r_validation_probs <- result_0_r$pi[validation_indices]
 end <- proc.time()
-end - begin
+end-begin
 
 truth_validation <- slice_5[validation_indices]
 
 library(pROC)
 
-roc(truth_validation, lsvd_probs_validation)
-lsvd_validation_mean_llk <- mean(truth_validation*log(lsvd_probs_validation)+
-                                   (1-truth_validation)*log(1-lsvd_probs_validation))
+roc(truth_validation, cxx_validation_probs)
+roc(truth_validation, r_validation_probs)
+mean(truth_validation*log(cxx_validation_probs)+
+                                   (1-truth_validation)*log(1-cxx_validation_probs))
+mean(truth_validation*log(r_validation_probs)+
+       (1-truth_validation)*log(1-r_validation_probs))
 
-roc(truth_validation, lcf_probs_validation)
-lcf_validation_mean_llk <- mean(truth_validation*log(lcf_probs_validation)+
-                                  (1-truth_validation)*log(1-lcf_probs_validation))
 
-#
-# begin <- proc.time()
-# nbmf_fit <- nbmf(Y= t(slice_0), Z=t(initial_mask), k=3, tol=1e-5)
-# nbmf_probs = t(nbmf_fit$pi)
-# end <- proc.time()
-# end - begin
+library(logisticPCA)
+
+# fit with logisticSVD
+begin <- proc.time()
+lsvd_fit <- logisticSVD(x=slice_0, k=4, conv_criteria=1e-5)
+lsvd_probs <- fitted(lsvd_fit, type="response")
+end <- proc.time()
+end - begin
+
+
+# train and provide validation loss
+begin <- proc.time()
+result_lcf <- train_lcf(train_mask = slice_0_train,
+                        validation_mask = slice_0_validation,
+                        observation = slice_5,
+                        K=4, lambda=1, max_iter=1000, tol=1e-5, cap=10)
+end <- proc.time()
+end - begin
+
+
