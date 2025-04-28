cabf83cdedde1b6e024bdfbc2ff7662f20f814ce
Mukai Wang <wangmk@umich.edu>
2025-04-28 15:23:13 -0400

    Add functionality of truncating probability estimation when proportion of uncensored entries are smaller than a certain threshold.

--- R/dfbm.R
+++ R/dfbm.R
@@ -7,7 +7,7 @@
 #' @param increment a value between 0 and 1, at least certain percentage of entries larger than one threshold is smaller than the next threshold
 #' @param cutoffs if a vector of cutoffs are provided, then no need to derive thresholds
 #' @param fix_Ks the user can fix the rank number for all the binary masks
-#' @param ignore if less than a certain proportion of entries are "observed" for a row or column, that row or column is ignored for matrix factorization
+#' @param ignore if less than a certain proportion of entries are "observed" for a  column, that column is ignored for matrix factorization
 #' @param max_K maximum number of ranks for binary matrix factorization
 #' @param lambdas ridge penalty parameters to try for the entries in the factorized matrices
 #' @param ncores number of cores for parallel computing, default 1
@@ -18,6 +18,10 @@
 dfbm <- function(count_mat, quantiles=seq(0.1, 0.9, 0.1),
                   increment=0.9, cutoffs=NULL, fix_Ks=NULL, max_K=10, ignore=0, lambdas=c(0.01, 0.1, 1),
                  ncores=1){
+
+  nsample <- nrow(count_mat)
+  nfeature <- ncol(count_mat)
+  maximum_threshold <- max(apply(count_mat, 2, quantile, probs=1-ignore))
 
   prevalences <- colMeans(count_mat > 0)
 
@@ -48,9 +52,11 @@ dfbm <- function(count_mat, quantiles=seq(0.1, 0.9, 0.1),
   } else{
     selected_thresholds <- cutoffs
   }
+  # remove automatically decided thresholds that were larger than the maximum value
+  selected_thresholds <- selected_thresholds[selected_thresholds <= maximum_threshold]
 
   if (!is.null(fix_Ks)){
-    stopifnot(length(fix_Ks) == length(selected_thresholds))
+    fix_Ks <- fix_Ks[1:length(selected_thresholds)]
   }
 
   print(sprintf("%d thresholds selected", length(selected_thresholds)))
@@ -71,20 +77,21 @@ dfbm <- function(count_mat, quantiles=seq(0.1, 0.9, 0.1),
 
     pi_mat <- matrix(0, nrow=nrow(count_mat), ncol=ncol(count_mat))
     rows_retain <- seq(1, nrow(count_mat))
-    ncols_retain <- seq(1, ncol(count_mat))
+    cols_retain <- seq(1, ncol(count_mat))
 
     # Do I need to subset rows and columns
-    row_means <- rowMeans(mask)
-    if (any(row_means <= ignore)){
-      rows_retain <- which(row_means > ignore)
-    }
     col_means <- colMeans(mask)
-    if (any(col_means <= ignore)){
+    if (any(col_means <= ignore)){ # remove genes which are already very censored
       cols_retain <- which(col_means > ignore)
     }
-    mask <- mask[rows_retain, cols_retain, drop=FALSE]
+    mask <- mask[, cols_retain, drop=FALSE]
+    row_means <- rowMeans(mask)
+    if (any(row_means == 0)){ # remove rows are already all censored
+      rows_retain <- which(row_means > 0)
+    }
+    mask <- mask[rows_retain, , drop=FALSE]
     observation <- observation[rows_retain, cols_retain, drop=FALSE]
-    print(dim(observation))
+
     if (nrow(observation) == 1 | ncol(observation) == 1){ # when the dimension of matrix reduce to 1, we can only take the mean
       pi_scalar <- sum(observation) / sum(mask)
       estimated_pi <- matrix(pi_scalar, nrow=nrow(observation), ncol=ncol(observation))
@@ -107,9 +114,11 @@ dfbm <- function(count_mat, quantiles=seq(0.1, 0.9, 0.1),
     } else{
       pi_mat <- estimated_pi
     }
-
     prob_mats[[j+1]] <- pi_mat
   }
+
+  cap_vals <- apply(count_mat, 2, quantile, probs=1-ignore/2)
+  cap_vals_mat <- matrix(rep(cap_vals, nsample), ncol=nsample) |> t()
 
   interval_vals <- rep(0, length(selected_thresholds))
   for (j in 1:(length(selected_thresholds)-1)){
@@ -128,6 +137,7 @@ dfbm <- function(count_mat, quantiles=seq(0.1, 0.9, 0.1),
   expected_counts <- matrix(0, nrow=nrow(count_mat), ncol=ncol(count_mat))
   for (j in 1:length(selected_thresholds)){
 
+    prev_threshold <- selected_thresholds[j]
     if (j==1){
       sprob1 <- prob_mats[[1]]
     } else{
@@ -135,11 +145,20 @@ dfbm <- function(count_mat, quantiles=seq(0.1, 0.9, 0.1),
     }
 
     if (j==length(selected_thresholds)){
-      sprob2 <- 0
+      sprob2 <- matrix(0, nrow=nsample, ncol=nfeature)
     } else{
       sprob2 <- sprob1*prob_mats[[j+1]]
     }
-    expected_counts <- expected_counts + interval_vals[j] * (sprob1 - sprob2)
+
+    tail_values_mat <- (cap_vals_mat + prev_threshold) / 2
+    estim_tail_mask <- sprob2 == 0 # whether probability of larger than the next threshold is zero
+    expected_counts[estim_tail_mask] <- expected_counts[estim_tail_mask] +
+      tail_values_mat[estim_tail_mask] * (sprob1[estim_tail_mask] - sprob2[estim_tail_mask])
+
+    if(!all(estim_tail_mask)){
+      expected_counts[!estim_tail_mask] <- expected_counts[!estim_tail_mask] +
+        interval_vals[j] * (sprob1[!estim_tail_mask] - sprob2[!estim_tail_mask])
+    }
 
   }
 
